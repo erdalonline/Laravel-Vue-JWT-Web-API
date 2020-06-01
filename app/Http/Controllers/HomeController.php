1fdfa968e22c513ef40e42606da3d9f6e4a9c51e
yerdal <yusuferdal@outlook.com>
2020-06-01 18:48:15 +0300

    composer json update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        echo "anasayfa";
+
+        return 'home';
+
     }
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -82,7 +82,7 @@ class UserController extends Controller
 
             return response ()->json ([
                 'error' => 'success',
-                'id' => $user->name,
+                'id' => $user->id,
                 'name'=> $user->name,
                 'role_id'=> $user->role_id,
                 'email'=> $user->email,
